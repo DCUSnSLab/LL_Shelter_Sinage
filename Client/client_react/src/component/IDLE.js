fb13ba4edf234e923d7b93912de4ab009140b0e8
ParkBoEun <108859861+PPOS2EUN@users.noreply.github.com>
2023-05-24 20:28:53 +0900

    Update IDLE.js

--- Client/client_react/src/component/IDLE.js
+++ Client/client_react/src/component/IDLE.js
@@ -3,7 +3,6 @@ import React, {useRef, useState, useEffect} from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from '../style/IDLE.module.css';
-import Header from "./header";
 import {Link, Route, Routes} from "react-router-dom";
 
 function IDLE() {

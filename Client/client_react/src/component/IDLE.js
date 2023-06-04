058fcfea447f69d0b275cfc1fbc964e84064f927
PPOS2EUN <soda1597@cu.ac.kr>
2023-06-04 17:20:10 +0900

    오류 발생 커밋 다시함

--- Client/client_react/src/component/IDLE.js
+++ Client/client_react/src/component/IDLE.js
@@ -151,6 +151,7 @@ export default function IDLE() {
                         </AnimatePresence>
                     </p>
                 </tr>
+                
             </div>
         </div>
     );

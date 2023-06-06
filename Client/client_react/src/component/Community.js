bfce7b38b2dd011d412b5f39a6a82a1319f907bd
PPOS2EUN <soda1597@cu.ac.kr>
2023-06-06 21:26:45 +0900

    LIV-396 framer_motion 삭제 및 npm install 에러 해결

--- Client/client_react/src/component/Community.js
+++ Client/client_react/src/component/Community.js
@@ -1,8 +1,6 @@
 import axios from "axios"
 import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
-import { motion, AnimatePresence } from 'framer-motion/dist/framer-motion';
-
 function Community() {
     const host_ip = `${process.env.REACT_APP_IP}`;
     const port = "8000";

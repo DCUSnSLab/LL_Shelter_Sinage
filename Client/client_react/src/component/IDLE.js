9b6849edb62256f576337f9adfbbf76c398b6559
PPOS2EUN <soda1597@cu.ac.kr>
2023-06-10 23:23:22 +0900

    LIV-394 working

--- Client/client_react/src/component/IDLE.js
+++ Client/client_react/src/component/IDLE.js
@@ -14,10 +14,46 @@ export default function IDLE() {
     const [socketConnected, setSocketConnected] = useState(false);
     let ws = useRef(null);
     const shelter_num = 7
-
     let timer = null;
     const [time, setTime] = useState(moment());
 
+
+    useEffect(() => {
+        if(!ws.current) {
+            ws.current = new WebSocket(addr);
+            ws.current.onopen = () => {
+                console.log("connected to " + addr);
+                setOutputs("connected to " + addr);
+                setSocketConnected(true);
+                ws.current.send(
+                    JSON.stringify({
+                        message: 0
+                    })
+                )
+            };
+            ws.current.onclose = (error) => {
+                console.log("disconnect from " + addr);
+                setOutputs("disconnect from " + addr)
+                console.log(error);
+            };
+            ws.current.onerror = (error) => {
+                console.log("connection error " + addr);
+                setOutputs("connection error " + addr)
+                console.log(error);
+            };
+            ws.current.onmessage = (evt) => {
+                // server에서 보낸 데이터
+                const data = JSON.parse(evt.data);
+                if(i===0){
+                    setImg(data)
+                    console.log(imgs);
+                    i++;
+                }
+            };
+        }
+    }, []);
+
+    let i = 0;
     useEffect(() => {
         timer = setInterval(() => {
             setTime(moment());
@@ -46,7 +82,6 @@ export default function IDLE() {
 
     function isVideo(filename) {
         let extV = getExtension(filename);
-        console.log(extV.toLowerCase())
         switch (extV.toLowerCase()) {
             case 'm4v':
             case 'avi':
@@ -57,51 +92,23 @@ export default function IDLE() {
         return false;
     }
 
-    useEffect(() => {
-        if(!ws.current) {
-            ws.current = new WebSocket(addr);
-            ws.current.onopen = () => {
-                console.log("connected to " + addr);
-                setOutputs("connected to " + addr);
-                setSocketConnected(true);
-                ws.current.send(
-                    JSON.stringify({
-                        message: 0
-                    })
-                )
-            };
-            ws.current.onclose = (error) => {
-                console.log("disconnect from " + addr);
-                setOutputs("disconnect from " + addr)
-                console.log(error);
-            };
-            ws.current.onerror = (error) => {
-                console.log("connection error " + addr);
-                setOutputs("connection error " + addr)
-                console.log(error);
-            };
-            ws.current.onmessage = (evt) => {
-                // server에서 보낸 데이터
-                const data = JSON.parse(evt.data)
-                data.map((data) => {
-                    addMessage(data);
-                    console.log(data);
-                })
-            };
-        };
-    }, []);
-
-    function addMessage(img) {
-
-        setImg([...imgs, img]);
-    }
-
     const settings = {
-        slide: 'div',
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrow: false
+        autoplaySpeed: 0, // Set to 0 to disable default autoplay behavior
+        speed: 1000, // Adjust the transition speed as per your preference
+    };
+    const sliderRef = useRef(null);
+
+    let inter;
+    const __image = setInterval(() => {
+            inter = sliderRef.current.slickNext();
+        }, 10000); // 10000 milliseconds = 10 seconds
+
+    const handleVideoEnd = () => {
+        // Go to the next slide when the video ends
+        sliderRef.current.slickNext();
     };
 
     return (
@@ -113,39 +120,34 @@ export default function IDLE() {
                 {/* LT=4:50 , LTS=4:50:21 */}
                 <div className={styles.page3_time}>{time.format('LT')}</div>
             </header>
-
-
-
-            <Slider {...settings}>
-                    {imgs.map(m =>
-                        <div className={styles.slide_list} key={m}>
-                            {
-                                isImage(m) === true ?
-                                <img src={`${process.env.PUBLIC_URL}` + m} key={m} />
-                                : <video muted autoPlay src={`${process.env.PUBLIC_URL}` + m} key={m} />
-                            }
-                        </div>
-                    )}
+            <Slider {...settings} ref={sliderRef} id="list">
+                {imgs.map((image, index) =>
+                    __image && isImage(image) === true && (
+                        <img id="img_list" src={`${process.env.PUBLIC_URL}` + image} />
+                    ) ||
+                    isVideo(image) === true && (
+                        <video id="video_list" onEnded={handleVideoEnd} muted autoPlay src={`${process.env.PUBLIC_URL}` + image}/>
+                    )
+                )}
             </Slider>
-
             <div className={styles.social}>
                 <tr>
                     <p>작품보기<br/>
-                        <button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} >
+                        <button>
                             <Link to='/select' style={{color : 'white', textDecoration: 'none'}}>GO</Link>
                         </button>
                     </p>
                 </tr>
                 <tr>
                     <p className={styles.line}>낙서장<br/>
-                        <button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} >
+                        <button>
                             <Link to='/board' style={{color : 'white', textDecoration: 'none'}}>GO</Link>
                         </button>
                     </p>
                 </tr>
                 <tr>
                     <p className={styles.QR}>
-                        <img whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1 }} key="shelter_num" src={`${process.env.PUBLIC_URL}` + '/ftp/ShelterQR/Content/Shelter_'+ shelter_num + '/contentQR.jpg'}/>
+                        <img key="shelter_num" src={`${process.env.PUBLIC_URL}` + '/ftp/ShelterQR/Content/Shelter_'+ shelter_num + '/contentQR.jpg'}/>
                     </p>
                 </tr>
             </div>
